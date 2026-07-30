a54a1ad7753d6ba4e6a14b8634dbec4ace0a8d00
tagnon-kpanou <harisdarenkpanou@gmail.com>
2026-07-29 20:28:37 -0400

    Complete Image Gallery functionality

---
+++ part2-image-gallery/main.js
@@ -0,0 +1,44 @@
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
+
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
+
+const images = [
+  { filename: "pic1.jpg", alt: "Closeup of a human eye" },
+  { filename: "pic2.jpg", alt: "Rock that looks like a wave" },
+  { filename: "pic3.jpg", alt: "Purple and white pansies" },
+  { filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb" },
+  { filename: "pic5.jpg", alt: "Large moth on a leaf" },
+];
+
+const baseURL = "images/";
+
+for (const image of images) {
+  const newImage = document.createElement("img");
+  newImage.src = baseURL + image.filename;
+  newImage.alt = image.alt;
+  newImage.setAttribute("tabindex", "0");
+  thumbBar.appendChild(newImage);
+
+  newImage.addEventListener("click", updateDisplayedImage);
+}
+
+function updateDisplayedImage(e) {
+  displayedImage.src = e.target.src;
+  displayedImage.alt = e.target.alt;
+}
+
+btn.addEventListener("click", () => {
+  const btnClass = btn.getAttribute("class");
+
+  if (btnClass === "dark") {
+    btn.textContent = "Lighten";
+    overlay.style.backgroundColor = "rgb(0 0 0 / 0.5)";
+    btn.setAttribute("class", "light");
+  } else {
+    btn.textContent = "Darken";
+    overlay.style.backgroundColor = "rgb(0 0 0 / 0)";
+    btn.setAttribute("class", "dark");
+  }
+});
